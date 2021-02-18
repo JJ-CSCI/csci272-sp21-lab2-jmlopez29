5e292854bfb5846e8fbbc1c4412a49d61cc6a1cd
jmlopez29 <jmlopez2992@gmail.com>
2021-02-18 18:23:47 +0000

    lab2

--- main.cpp
+++ main.cpp
@@ -6,9 +6,61 @@
 
 // Use this enum class for indicating the lat/long direction
 enum class Compass {N, S, W, E};
+private:
+getLatitude = double N,S;
+getLatitudeDirection = Compass N,S;
+
+getLongitude = double W,E;
+getLongitudeDirection = Compass W,E;
+
+int main(){
+  getLatitude{};
+  double N.print();
+  double S.print();
+
+  getLatitudeDirection{};
+  Compass N.print();
+  Compass S.print();
+
+  getLongitude{};
+  double W.print();
+  double E.print();
+
+  getLongitudeDirection{};
+  double W.print();
+  double E.print();
+
+}
 
 // Write your code here
 class GPS {
+  getLatitude [0.0, 90.0]
+  getLatitude = Compass::N,Compass::S;
+
+  getLongitude [0.0, 180.0]
+  getLongitude = Compass::W, Compass::E;
+
+  if{
+    (Coordinates == 0;)
+    getLatitude = 0;
+    getLongitude = 0;
+    getLatitude = Compass N;
+    getLongitude = Compass W;
+  }
+  if{
+    getLatitude = object(){
+      Compass N.print();
+    }
+    getLongitude = object(){
+      Compass W.print();
+    }
+  }
+  if{
+    getLatitude = getLatitude < getLatitude;
+    Coordinate = Compass N;
+    getLongitude = getLongitude < getLongitude;
+    Coordinate = Compass W;
+  }
 
 };
 
